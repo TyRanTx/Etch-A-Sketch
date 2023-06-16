670a0f4d90c791adbcc44a4382439a7f6895a3ad
Samir Qureshi <samir.h.qureshi@gmail.com>
2023-06-16 21:28:51 +0500

    Refactor code

--- script.js
+++ script.js
@@ -6,23 +6,19 @@ let randomCol = 'false';
 let currentBrightness;
 let mouse_down = 'false';
 
-
 const canvas = document.querySelector('.canvas');
 
-document.addEventListener('mousedown', () => { mouse_down = 'true'; console.log('Mouse down:' + mouse_down);})
-document.addEventListener('mouseup', () => { mouse_down = 'false'; console.log('Mouse down:' + mouse_down);})
-
+document.addEventListener('mousedown', () => mouse_down = 'true');
+document.addEventListener('mouseup', () => mouse_down = 'false');
 
 initializeSquares();
 hoverSquares();
 
-/* Adds squares in container. First it removes old squares that are already present in the container, and then goes through loop to add one by one.
+/* Adds squares in container. First it removes old squares that are already present in the canvas, and then goes through loop to add one by one.
 Default size is 16x16 */
 
 function initializeSquares() {
-
     removePreviousSquares();
-
     for (let i = 0; i < canvasSize * canvasSize; i++) {
         const box = document.createElement('div');
         box.classList.add('square');
@@ -35,44 +31,49 @@ function initializeSquares() {
     }
 }
 
-
-
 function hoverSquares() {
-
     const square = canvas.querySelectorAll('*');
-
-    square.forEach(element => element.addEventListener('mouseover', () => {
-        
+    square.forEach(e => e.addEventListener('mouseover', () => {
         if (mouse_down === 'true') {
-            if (!(element.classList.contains('square-hover'))) {
-                if (randomCol === 'false') {
-                    element.classList.add('square-hover');
-                    element.style.backgroundColor = '#FF6D60';
-                }
-                else {
-                    elightement.classList.add('square-hover');
-                    element.style.backgroundColor = randomColor();
-                }
+            if (!(e.classList.contains('filled'))) {
+                fillSquare(e);
             }
-
-            if (element.classList.contains('square-hover')) {
-                if (dark === 'true') {
-                    darken(element);
-                }
-                else if (light === 'true') {
-                    lighten(element);
-                }
+            if (e.classList.contains('filled')) {
+                changeBrightness(e);
             }
-
             if (eraser === 'true') {
-                element.classList.remove('square-hover');
-                element.style.backgroundColor = '';
-                element.style.filter = 'brightness(100%)';
-
+                erase(e);
             }
-       }
-    }))
+        }
+    }
+    )
+    )
+}
 
+function erase(e) {
+    e.classList.remove('filled');
+    e.style.backgroundColor = '';
+    e.style.filter = 'brightness(100%)';
+}
+
+function fillSquare(e) {
+    if (randomCol === 'false') {
+        e.classList.add('filled');
+        e.style.backgroundColor = '#FF6D60';
+    }
+    else {
+        e.classList.add('filled');
+        e.style.backgroundColor = randomColor();
+    }
+}
+
+function changeBrightness(e) {
+    if (dark === 'true') {
+        darken(e);
+    }
+    else if (light === 'true') {
+        lighten(e);
+    }
 }
 
 function removePreviousSquares() {
@@ -81,21 +82,21 @@ function removePreviousSquares() {
     }
 }
 
-// darks square by 10%
+// Darken square by 10%
 
-function darken(element) {
-    currentBrightness = element.style.filter.replace(/\D/g, "");
+function darken(e) {
+    currentBrightness = e.style.filter.replace(/\D/g, "");
     if (currentBrightness > 0) {
-        element.style.filter = `brightness(${currentBrightness - 10}%)`;
+        e.style.filter = `brightness(${currentBrightness - 10}%)`;
     }
 }
 
-// lights square by 10%
+// Lighten square by 10%
 
-function lighten(element) {
-    currentBrightness = Number(element.style.filter.replace(/\D/g, ""));
+function lighten(e) {
+    currentBrightness = Number(e.style.filter.replace(/\D/g, ""));
     if (currentBrightness < 100) {
-        element.style.filter = `brightness(${currentBrightness + 10}%)`;
+        e.style.filter = `brightness(${currentBrightness + 10}%)`;
     }
 }
 
@@ -108,31 +109,33 @@ function randomColor() {
     return `#${randcolor.toUpperCase()}`;
 }
 
-// HTML button functions
+// Button functions
 
-const btndark = document.querySelector('.btnDarken');
-const btnlight = document.querySelector('.btnLighten');
-const btnReset = document.querySelector('.btnReset');
-const btn_eraser = document.querySelector('.btnEraser');
-const btn_color = document.querySelector('.btnRandomColor');
+const btndark = document.querySelector('#btnDarken');
+const btnlight = document.querySelector('#btnLighten');
+const btnReset = document.querySelector('#btnReset');
+const btn_eraser = document.querySelector('#btnEraser');
+const btn_color = document.querySelector('#btnRandomColor');
 
 function setCanvas() {
     let size = prompt('Please enter grid size:', '5');
-    
-    if(size === null){
-        console.log(size); 
+    if (size === null) {
+        console.log(size);
         return;
     }
-    else{
-   canvasSize = parseInt(size);
-        console.log(canvasSize); 
-   initializeSquares();
-    hoverSquares();
-}
+    else {
+        canvasSize = parseInt(size);
+        console.log(canvasSize);
+        initializeSquares();
+        hoverSquares();
+    }
 }
 
 function toggleDarken() {
-    if (dark === 'false' && light === 'false' && eraser === 'false' && randomCol === 'false') {
+    if (dark === 'false' &&
+        light === 'false' &&
+        eraser === 'false' &&
+        randomCol === 'false') {
         btndark.style.backgroundColor = '#D67D3E';
         dark = 'true';
     }
@@ -143,11 +146,13 @@ function toggleDarken() {
 }
 
 function toggleLighten() {
-    if (light === 'false' && dark === 'false' && eraser === 'false' && randomCol === 'false') {
+    if (light === 'false' &&
+        dark === 'false' &&
+        eraser === 'false' &&
+        randomCol === 'false') {
         btnlight.style.backgroundColor = '#D67D3E';
         light = 'true';
         dark = 'false';
-
     }
     else {
         btnlight.removeAttribute("style");
@@ -171,7 +176,7 @@ function reset() {
     let counter = 0;
 
     while (counter < canvas.childElementCount) {
-        canvas.children[counter].classList.remove('square-hover');
+        canvas.children[counter].classList.remove('filled');
         canvas.children[counter].style.backgroundColor = '';
         canvas.children[counter].style.filter = 'brightness(100%)';
         counter++;
@@ -179,31 +184,29 @@ function reset() {
 }
 
 function toggleEraser() {
-
-    if (eraser === 'false' && dark === 'false' && light === 'false' && randomCol === 'false') {
+    if (eraser === 'false' &&
+        dark === 'false' &&
+        light === 'false' &&
+        randomCol === 'false') {
         btn_eraser.style.backgroundColor = '#D67D3E';
         eraser = 'true'
     }
     else {
         btn_eraser.removeAttribute("style");
         eraser = 'false';
-
     }
 }
 
 function toggleRandomColor() {
-    if (randomCol === 'false' && dark === 'false' && light === 'false' && eraser === 'false') {
+    if (randomCol === 'false' &&
+        dark === 'false' &&
+        light === 'false' &&
+        eraser === 'false') {
         btn_color.style.backgroundColor = '#D67D3E';
         randomCol = 'true'
     }
     else {
         btn_color.removeAttribute("style");
         randomCol = 'false';
-
     }
 }
-
-
-
-
-
